6be759a2bb2380d714db5bb25f7beb17354c93f7
dsatge <deborah.satge@yahoo.fr>
2025-04-08 19:01:04 +0200

    loop for mult heredoc, still has issue as does do the last command when mult

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: dsatge <dsatge@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:38:17 by enschnei          #+#    #+#             */
-/*   Updated: 2025/04/08 17:51:44 by dsatge           ###   ########.fr       */
+/*   Updated: 2025/04/08 18:57:13 by dsatge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,12 @@ static void creat_heredoc(t_pipe **pipex, t_list *cmds)
 {
     char	*buffer;
 	char	*path_cmd;
-	int	i;
     int fd;
 
-	i = 0;
 	path_cmd = NULL;
     (void)pipex;
     fd = open("File_heredoc", O_RDWR | O_TRUNC | O_CREAT, 0644);
-    if (fd == -1)
+	if (fd == -1)
         return ;
     while (1)
     {
@@ -44,18 +42,20 @@ static void creat_heredoc(t_pipe **pipex, t_list *cmds)
         }
         if (!ft_strcmp(buffer, cmds->cmd->tab[1]))
         {
-            close(fd);
+            free(buffer);
             break;
         }
         ft_putstr_fd(buffer, fd);
         write(fd, "\n", 1);
         free(buffer);
     }
+	close(fd);
+	exit(EXIT_SUCCESS);
 }
 
 int heredoc(t_pipe **pipex, t_list *cmds)
 {
-    int		pid;
+    int		pid_heredoc;
     int		status;
     t_list	*list;
     
@@ -65,22 +65,23 @@ int heredoc(t_pipe **pipex, t_list *cmds)
     {
 		if (list->cmd->type == redir && ft_strcmp(list->cmd->tab[0], "<<") == 0)
 		{
-			pid = fork();
-		    if (pid == -1)
+			pid_heredoc = fork();
+		    if (pid_heredoc == -1)
 			{
 				perror("Error fork heredoc");
 				return (EXIT_FAILURE);
 			}
-			if (pid == 0)
+			if (pid_heredoc == 0)
 			{
 				signal(SIGINT, close_fd);
 				creat_heredoc(pipex, list);
 				exit(EXIT_SUCCESS);
 			}
+			waitpid(pid_heredoc, &status, 0);
 		}
 		list = list->next;
 	}
-    waitpid(pid, &status, 0);
+    // waitpid(pid_heredoc, &status, 0);
     (*pipex)->infile_fd = open("File_heredoc", O_RDONLY);
     if ((*pipex)->infile_fd == -1)
         return (EXIT_FAILURE);
